94376f4816cb203131e9481d6b143ab1e4dd5c80
Sreekanth Rajan <v-srajan@expedia.com>
2020-12-13 02:36:16 -0800

    Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -6,7 +8,10 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Dicee'),
+          centerTitle: true,
+          title: Text(
+            'Dicee',
+          ),
           backgroundColor: Colors.red,
         ),
         body: DicePage(),
@@ -15,9 +20,45 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftdicenumber = 1;
+  int rightdicenumber = 1;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            flex: 1,
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  leftdicenumber = Random(1).nextInt(6);
+                  print(leftdicenumber);
+                });
+              },
+              child: Image.asset("images/dice$leftdicenumber.png"),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  rightdicenumber = Random(1).nextInt(6);
+                });
+              },
+              child: Image.asset("images/dice$rightdicenumber.png"),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
